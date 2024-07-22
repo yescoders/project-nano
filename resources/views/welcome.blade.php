cb48aafe5dcc14671ad256c329cd238607ea13d7
Monir Hossain <monir.hossain@check24.de>
2024-07-22 21:24:26 +0200

    NANO-8 Add vue with laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
- Welcome to Project Nano
+<!DOCTYPE html>
+<head>
+    {{-- ... --}}
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body id="app" class="font-sans antialiased">
+Welcome to Project Nano
+<increment-counter />
+
+</body>
+
