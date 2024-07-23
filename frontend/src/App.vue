2c9b6892264dd699b32ae0188d94cc9ce6806d04
Monir Hossain <monir.hossain@check24.de>
2024-07-23 22:57:43 +0200

    NANO-12 nav background

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,13 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-    <h1 class="bg-amber-400">Monir</h1>
-    <nav class="bg-amber-400">
-        <RouterLink to="/" class="px-8">Home</RouterLink>
-        <RouterLink to="/login" class="p-1">Login page</RouterLink>
-        <RouterLink to="/register" class="p-1">Register page</RouterLink>
-        <RouterLink to="/dashboard" class="p-1">Dashboard</RouterLink>
+    <nav class="bg-green-500">
+        <RouterLink to="/" class="p-2">Home</RouterLink>
+        <RouterLink to="/login" class="p-2">Login page</RouterLink>
+        <RouterLink to="/register" class="p-2">Register page</RouterLink>
+        <RouterLink to="/dashboard" class="p-2">Dashboard</RouterLink>
     </nav>
-    <div>
+    <div class="p-5">
         <RouterView />
     </div>
 </template>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/frontend/',
   server: {
     port: 8001,
     host: '0.0.0.0'
