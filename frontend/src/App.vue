b97ad10d23330c768c3071278a22ab753c4cec21
Monir Hossain <monir.hossain@check24.de>
2024-07-23 21:58:00 +0200

    NANO-12 cleanup default files and styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TodoList from './components/TodoList.vue'
-
+import TodoList from './components/TodoList.vue';
 
 </script>
 
@@ -9,17 +7,6 @@ import TodoList from './components/TodoList.vue'
     <div class="p-6 bg-blue-500 text-white my-class">
         Hello, Tailwind with SCSS!
     </div>
-
-    <h1>Hello world</h1>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
   <TodoList />
 </template>
 
@@ -27,17 +14,5 @@ import TodoList from './components/TodoList.vue'
 .my-class {
     @apply text-center; // Using Tailwind utilities in SCSS
     color: red;
-}
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import './assets/tailwind.scss'
+import './assets/style.scss'
 
 createApp(App).mount('#app')
